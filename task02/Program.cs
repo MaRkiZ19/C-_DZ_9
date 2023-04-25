ff4c64fedfdb687fdbd1b9ea2c76b2badaa19b9c
ваше_имя <grigorenko-marko@rambler.ru>
2023-04-25 22:42:14 +0300

    task02 В соответствии с ТЗ, программа без рекурсии

---
+++ task02/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+int Num (string arg)
+{
+    System.Console.WriteLine($"Введите {arg} ");
+    return int.Parse(System.Console.ReadLine());
+}
+
+int M = Num("M");
+int N = Num("N");
+
+int Summ(int M, int N)
+{
+    int sum = 0;
+    for (int i = M; i <= N; i++)
+    {
+        sum = sum + i;
+    }
+return sum;
+}
+
+System.Console.WriteLine(Summ(M, N));
